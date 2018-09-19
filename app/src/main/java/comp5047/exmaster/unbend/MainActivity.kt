431ed5b51d22bbee8545bca9e2e80bcb9134c6ca
Ken <kensumetvarapa@gmail.com>
2018-09-19 15:38:41 +1000

    Init

---
+++ app/src/main/java/comp5047/exmaster/unbend/MainActivity.kt
@@ -0,0 +1,12 @@
+package comp5047.exmaster.unbend
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
